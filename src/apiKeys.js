20191fddbacb6f06907833ac9866f30b07de435c
GITAkash12 <165632975+GITAkash12@users.noreply.github.com>
2024-06-05 10:04:06 +0530

    Update apiKeys.js

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,6 +1,6 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 const apiKeys = {
-  key: "cf03ee726c2843762f0c10d851391e8b",
+  key: "{}",
   base: "https://api.openweathermap.org/data/2.5/",
 };
 export default apiKeys;
