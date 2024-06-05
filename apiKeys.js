2fa2b1eb2fadfc80e262fcd5d9cebc0ce74e7b95
GITAkash12 <beingakash12@gmail.com>
2024-06-05 09:55:55 +0530

    deleted everything

--- apiKeys.js
+++
@@ -1,6 +0,0 @@
-// Visit https://api.openweathermap.org & then signup to get our API keys for free
-const apiKeys = {
-  key: "{}",
-  base: "https://api.openweathermap.org/data/2.5/",
-};
-export default apiKeys;
